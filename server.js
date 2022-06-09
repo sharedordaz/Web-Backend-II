5d2cacfe68ae062f833ec8114252e2220b0b2c65
sharedordaz <sharedordaz@live.com.mx>
2022-06-08 23:21:55 -0500

    index.js

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) =>{
-    res.send("hello World!");
-})
+//#We don't want to use all our code in a file
+// app.get('/', (req, res) =>{
+//     res.send("hello World!");
+// })
+
+
+app.use('/', require('./routes'));
 
 app.listen(port, () =>{
     console.log(`Example app listening on port ` + port)
