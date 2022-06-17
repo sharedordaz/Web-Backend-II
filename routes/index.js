a30c762918f3fc1f7325dbeb630bff7ab1696857
sharedordaz <sharedordaz@live.com.mx>
2022-06-16 20:15:31 -0500

    added .gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const routes = require('express').Router();
+const routes = require('express').Router(); //MIDDLEWARE IS A CONTROLLER (Within the paradigm of View-Controller-Model)
 
 routes.get("/", (req, res) =>{
     res.send('Hello, Sarah Birch')
